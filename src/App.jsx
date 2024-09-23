1e2bb5a86c8601871994afe91c6bf43889837eb5
Brandon Hamilton <bham2015@gmail.com>
2024-09-23 13:25:42 -0500

    I'm going to eat.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { puppyList } from "./data.js";
 import { useState } from "react";
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   
   console.log(puppies);
   return (
@@ -10,9 +11,10 @@ function App() {
       <div className="App">
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
+        <p>{featPupId}</p>
       </div>
     </>
   )
